2fa4a5ec25f3a805c3f6f22697a657c2d3409457
roddg <roddg86@gmail.com>
2020-10-03 13:32:31 +0900

    1.2 meals list display to JSP

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,23 +1,30 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.meals;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final int DEFAULT_CALORIES_PER_DAY = 2000;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("dateTimeFormatter", formatter);
+        request.setAttribute("meals", MealsUtil.filteredByStreams(meals, LocalTime.MIN, LocalTime.MAX, DEFAULT_CALORIES_PER_DAY));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,11 +1,34 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
-<html lang="ru">
+<html lang="en">
 <head>
     <title>Meals</title>
 </head>
 <body>
-<h3><a href="index.html">Home</a></h3>
+<h3>
+    <a href="index.html">Home</a>
+</h3>
 <hr>
 <h2>Meals</h2>
+<table border="1">
+    <thead>
+    <tr>
+        <td>Дата/Время</td>
+        <td>Описание</td>
+        <td>Калории</td>
+    </tr>
+    </thead>
+
+    <jsp:useBean id="dateTimeFormatter" scope="request" type="java.time.format.DateTimeFormatter"/>
+    <jsp:useBean id="meals" scope="request" type="java.util.List<ru.javawebinar.topjava.model.MealTo>"/>
+    <c:forEach var="mealTo" items="${meals}">
+        <jsp:useBean id="mealTo" type="ru.javawebinar.topjava.model.MealTo" />
+        <tr style="color: ${mealTo.excess ? "red" : "green"}">
+            <td>${dateTimeFormatter.format(mealTo.dateTime)}</td>
+            <td>${mealTo.description}</td>
+            <td>${mealTo.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
